031db63e997365873730ba64d179fbe4c5dcfd69
NicolasAndreiDev <nicolasandreislc@gmail.com>
2024-03-08 15:23:33 -0300

    testando

--- d2/front/src/App.js
+++ d2/front/src/App.js
@@ -1,7 +1,32 @@
 function App() {
+  const list = new Array(4).fill({
+    nome: "Nicolas Andrei da Silva", 
+    placa: "XXX-1234", 
+    telefone: "(123) 456-7890",
+    email: "motoboy@example.com"
+  });
+
   return (
     <div>
-      
+      <header>
+      <h1>Gestão de Motoboys</h1>
+      </header>
+      <section>
+        {list.map((card) => {
+          return(
+            <div class="motoboy-card">
+              <h2>{card.nome}</h2>
+              <p>Placa: {card.placa}</p>
+              <p>Telefone: {card.telefone}</p>
+              <p>Email: {card.email}</p>
+              <div class="motoboy-actions">
+                <button>Editar</button>
+                <button>Excluir</button>
+              </div>
+            </div>
+          )
+        })}
+      </section>
     </div>
   );
 }
