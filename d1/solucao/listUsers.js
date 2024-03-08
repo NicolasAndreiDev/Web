9378d82793070bda3e4ba084b1c706312d71d0ca
NicolasAndreiDev <nicolasandreislc@gmail.com>
2024-03-08 13:25:57 -0300

    d1

---
+++ d1/solucao/listUsers.js
@@ -0,0 +1,19 @@
+const ListUsers = [
+    {   
+        nome: "Nicolas",
+        idade: 19,
+        email: "nicolasandreislc@gmail.com"
+    },
+    {   
+        nome: "Matheus",
+        idade: 30,
+        email: "matheus@gmail.com"
+    },
+    {   
+        nome: "Gustavo",
+        idade: 22,
+        email: "gustavo@gmail.com"
+    }
+]
+
+export default ListUsers;
